06c09444ac37a6574d92da66a58b204c631d0938
niteshcvg <nitesh@convrg.io>
2018-08-18 20:30:46 +0530

    Facebook messenger bot for selling phone back case online

--- confidential/data.js
+++ confidential/data.js
@@ -1,11 +1,11 @@
 const port = '1234',
-      accessToken = '',
-      verifyToken = '';
+      accessToken = 'EAAGa4ywgfZBcBAAjGpkjJZCgCj477xQEiGNlx7lO7Ew0SsDNcHO2nwQWdOqkWCCxgITdgSpcaNr5spZAjgfv6pGNVwhSm7MmpcIX381BmyXrjB4cEwRFcPkiZBLzlR2OXQmGcVZCEThaiDnS6dtp5OUyICpyS5xw3WxpyzzTIJwZDZD',
+      verifyToken = 'nitesh';
 
 const DB_HOST = 'localhost',
       DB_USER = 'root',
       DB_PASS = '',
-      DB_NAME = '',
+      DB_NAME = 'nickscomputer',
       DB_PORT = '3306';
 
 module.exports = {
@@ -19,3 +19,9 @@ module.exports = {
   DB_PORT : DB_PORT
 }
 
+// curl - X POST - H "Content-Type: application/json" - d '{
+// "whitelisted_domains": [
+//       "https://56c78928.ngrok.io"
+// ]
+// }
+// ' "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=EAAGa4ywgfZBcBAAjGpkjJZCgCj477xQEiGNlx7lO7Ew0SsDNcHO2nwQWdOqkWCCxgITdgSpcaNr5spZAjgfv6pGNVwhSm7MmpcIX381BmyXrjB4cEwRFcPkiZBLzlR2OXQmGcVZCEThaiDnS6dtp5OUyICpyS5xw3WxpyzzTIJwZDZD"
